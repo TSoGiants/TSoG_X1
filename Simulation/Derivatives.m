06a0894507f717b10d2412fbf40733a744029dae
Nufflee <zifcani@gmail.com>
2020-06-20 00:39:08 +0200

    Add simple drag & lift to the simulation.

--- Simulation/Derivatives.m
+++ Simulation/Derivatives.m
@@ -1,9 +1,25 @@
-function StateVector_Dot = Derivatives(StateVector)
+function StateVector_Dot = Derivatives(StateVector, Object)
+  Velocity = P_dot = StateVector(3:4); % Derivative of position is velocity
+  Speed    = norm(Velocity);           % Magnitude of velocity vector
+
+  Gravity = [0, -9.81]; % Acceleration due to gravity
+
+  Density = 1.225;                              % Standard air density (kg/m3)
+  Q       = 0.5 * Density * Speed^2;            % Dynamic pressure
+  Drag    = Object.DragRefArea * Object.Cd * Q; % Drag force
+  Lift    = Object.LiftRefArea * Object.Cl * Q; % Lift force
   
-  P_dot = StateVector(3:4);
+  if Speed == 0
+    Drag_dir = [0, 0];
+  else
+    Drag_dir = -Velocity / Speed; % Drag direction - normalized velocity vector
+  endif
   
-  V_dot = [0 -9.81];
+  Lift_dir = Drag_dir * [0 -1; 1 0];
   
-  StateVector_Dot = [P_dot V_dot];
-  
+  F = Drag_dir * Drag + Lift_dir * Lift; % Net force on the object
+ 
+  V_dot = Gravity + F / Object.Mass; % Derivative of velocity is acceleration
+
+  StateVector_Dot = [P_dot, V_dot];
 endfunction
--- Simulation/Main.m
+++ Simulation/Main.m
@@ -1,30 +1,36 @@
-% Base off of https://github.com/TSoGiants/MECH2103-Computational-Aerodynamics
+% Based off of https://github.com/TSoGiants/MECH2103-Computational-Aerodynamics
 
 % Object parameters
-Plane.Mass = 1;
+Plane.Mass        = 1; % Mass (kg)
+Plane.DragRefArea = 1; % Cross sectional area used for drag (m2)
+Plane.LiftRefArea = 0; % Projected wing area used for lift (m2)
+Plane.Cd          = 1; % Coefficient of drag (dimensionless)
+Plane.Cl          = 0; % Coefficient of lift (dimensionless)
+
+printf('Terminal velocity: %d m/s\n', sqrt((2 * Plane.Mass * 9.81) / (Plane.Cd * 1.225 * Plane.DragRefArea)));
 
 % State vector initialization
 % X - distance, Y - height
 P0 = [0, 10]; % Initial position (m)
-V0 = [-2, 0]; % Initial velocity (m/s)
+V0 = [0, 0]; % Initial velocity (m/s)
 
-StateVector = [P0 V0];
+StateVector = [P0, V0];
 
 % Results used for plotting
-Results.X  = StateVector(1);
-Results.Y  = StateVector(2);
-Results.Vx = StateVector(3);
-Results.Vy = StateVector(4);
+Results.X    = StateVector(1);
+Results.Y    = StateVector(2);
+Results.Vx   = StateVector(3);
+Results.Vy   = StateVector(4);
 Results.Time = 0;
 
 % Simulation parameters
 dt = 0.05;         % Step time (s)
-end_time = 10;     % Simulation end time(s)
+end_time = 10;     % Simulation end time (s)
 ground_height = 0; % Height of the ground (m)
 
-for i=2:(end_time / dt)  
+for i=2:(end_time / dt)
   % Integrate next step
-  StateVector = RK4_Integration(StateVector,dt);
+  StateVector = RK4_Integration(StateVector, Plane, dt);
   
   % Save Results
   Results.X(i)    = StateVector(1);
@@ -35,13 +41,16 @@ for i=2:(end_time / dt)
   
   % Check if object has hit the ground
   if StateVector(2) < ground_height
+    printf('Ground hit in %d s\n', Results.Time(end))
     break;
   endif
 endfor
 
-plot(Results.X, Results.Y)
-xlabel('X position (m)')
-ylabel('Y position (m)')
-set(legend('Trajectory'), "fontsize", 25)
+plot(Results.Time, Results.Y)
+hold on
+plot(Results.Time, Results.Vy)
+xlabel('Time (s)')
+%ylabel('Y position (m)')
+set(legend('Trajectory (m)', 'Velocity (m/s)'), "fontsize", 25)
 set(gca, "fontsize", 25)
 grid on

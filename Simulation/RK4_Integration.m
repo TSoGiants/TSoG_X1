4bf00607dc9502c0b89af83f9923f9a04e2de405
TheGreatFez <raul.maldonado83@gmail.com>
2020-06-27 14:58:05 -0500

    Update RK4_Integration.m
    ajsdfasld
    afaskdjfasasdfa
    asdflkfjaklsdf
    asdfasasdfassdfa

--- Simulation/RK4_Integration.m
+++ Simulation/RK4_Integration.m
@@ -4,6 +4,11 @@ function StateVector = RK4_Integration(StateVector, Object, dt)
   k2 = dt * Derivatives(StateVector + k1/2, Object);
   k3 = dt * Derivatives(StateVector + k2/2, Object);
   k4 = dt * Derivatives(StateVector + k3, Object);
+fdasdfsdfasdfasda
+asdfasdfasd
+fasdfasd
+asdfZXCacADXCC
+asdf
 
   StateVector = StateVector + (1/6) * (k1 + 2*k2 + 2*k3 + k4);
 endfunction

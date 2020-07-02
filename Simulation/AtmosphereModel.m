cd5db52718e92894c99a062e2d607d2fc90cee5b
Szydlik <szydlikr@lisd.net>
2020-07-02 15:58:48 -0500

    Update AtmosphereModel.m
    Updated explanation of what the code means and represents in relation to the atmosphere model.

--- Simulation/AtmosphereModel.m
+++ Simulation/AtmosphereModel.m
@@ -1,7 +1,15 @@
 % Atmosphere model 
-% <EXPAND ON WHAT THE CODE IS DOING HERE> 
 
-% atmosphere model is sourced from NASA at this link:
+% This code represents how we can approximate the temperature (degrees Celsius),
+% pressure (kPa), and density (kg / m ^ 3) as a function of height/altitude (meters).
+% Based off the NASA atmospheric model, the atmosphere that planes travel in can be 
+% broken up into three sections: troposphere, lower stratosphere, and upper stratosphere.
+% For the reality of this RC plane, we will only work on the model regarding the
+% troposphere constants. In the troposphere, temperature decreases linearly, while 
+% pressure decreases exponentially, and density is based off the values of 
+% temperature and pressure.
+
+% Atmosphere model is sourced from NASA at this link:
 % https://www.grc.nasa.gov/WWW/K-12/airplane/atmosmet.html
 
 % Create input altitude

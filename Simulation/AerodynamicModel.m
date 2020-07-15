ea91556c5fb9df7ef026a89916f393bb8c3df1e5
Nufflee <zifcani@gmail.com>
2020-07-15 04:43:31 +0200

    Make Cd and Cl functions of AoA
    This doesn't seem to work yet because there are some AoA anomalies.

--- Simulation/AerodynamicModel.m
+++ Simulation/AerodynamicModel.m
@@ -4,8 +4,8 @@ function [ Drag, Lift ] = AerodynamicModel(StateVector, Object)
   
   Density = 1.225;                              % Standard air density (kg/m3)
   Q       = 0.5 * Density * Speed^2;            % Dynamic pressure
-  Drag    = Object.AeroRefArea * Object.Cd * Q; % Drag force
-  Lift    = Object.AeroRefArea * Object.Cl * Q; % Lift force
+  Drag    = Object.AeroRefArea * Object.Cd(Object.AoA) * Q; % Drag force
+  Lift    = Object.AeroRefArea * Object.Cl(Object.AoA) * Q; % Lift force
 
   if Speed == 0
     Drag_dir = [0, 0];

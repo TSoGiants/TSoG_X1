1e53a1cc6c718d464f364367afbcff5935c5fc6a
ishankumar1011 <66332436+ishankumar1011@users.noreply.github.com>
2020-07-21 17:08:49 -0500

    Added structures with dummy variables

--- Simulation/Convert_StateVector.m
+++ Simulation/Convert_StateVector.m
@@ -1,1 +1,11 @@
 %Convert_StateVector
+
+SimData.Plane.Mass = 1
+SimData.Plane.AeroRefArea = 1
+SimData.Plane.Cd = 1
+SimData.Plane.Cl = 1
+SimData.Plane.AoA = 1
+SimData.StateVector.Position = [0,0]
+SimData.StateVector.Velocity = [0,0]
+SimData.StateVector.Orientation = [Pitch]
+SimData.Time = 0

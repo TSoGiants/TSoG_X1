986ab6b62177b7a96bb0ab988a060e311d14fe1a
Aditya Ojha <35882252+adiojha629@users.noreply.github.com>
2020-08-10 10:07:15 -0500

    Update ThrustModel.m
    Throttle must be a percentage, representing the efficiency of the motor at the given time

--- Simulation/ThrustModel.m
+++ Simulation/ThrustModel.m
@@ -1,16 +1,31 @@
 %Thrust Model
 
 %This code represents how thrust is caclculated in the x and y directions
-%Thrust is made up of "MaxThrust", "Throttle", and "airspeed"
+%Thrust calculated from RPM and Airspeed. The equation used can be found here:
+%https://www.electricrcaircraftguy.com/2014/04/propeller-static-dynamic-thrust-equation-background.html
 %This function returns an array of Thrust in the x and y directions
 
 function Thrust = ThrustModel(StateVector, Throttle)
+  %constants we get from propeller 
+  diameter = 6; %inches
+  pitch = 3; %inches (how far forward propeller moves in one rotation
+  Kv = 2280; %RPM per volt
+  Max_Volt = 11.1; %volts
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %Note that Kv*Max_Volt = Max Theoretical RPM.
+  %To scale down this RPM, we will use the 'Throttle' input, which is a percentage
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  %calculated values
   airspeed = sqrt(StateVector(3)^2 + StateVector(4)^2);
-  MaxThrust = 100;
-  Pitch = StateVector(5);
-  #Thrust = MaxThrust*Throttle*(1-airspeed/50); #old equations
-  Thrust = 1.704 - 0.1342*airspeed #based on RPM of 10,000 Rotations per minute
+  RPM = Max_Volt*Kv*Throttle;%use Throttle to scale down the max RPM 
+  Thrust = 4.392399*10^(-8)*RPM*diameter^(3.5)*pitch^(-.5)*((4.23333*10^(-4))*RPM*pitch-airspeed)%calculate thrust (based on model in excel file)
+  
+  %Calculate X,Y directions
+  Pitch = StateVector(5); %this is an angle in degrees
   Thrust_x = Thrust*cosd(Pitch);
   Thrust_y = Thrust*sind(Pitch);
+  %Return value
   Thrust = [Thrust_x,Thrust_y];
 endfunction

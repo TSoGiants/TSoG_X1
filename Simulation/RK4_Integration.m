f9729901b4889cb79cbfd72df1bb0b0d395ccbcf
Raul Maldonado <rmaldonado01@bellflight.com>
2020-08-25 13:57:27 -0500

    Removed comment from line 8. Added B_dot to kN structures. Added B_delta to avg_delta structure

--- Simulation/RK4_Integration.m
+++ Simulation/RK4_Integration.m
@@ -5,8 +5,9 @@ function SimData = RK4_Integration(SimData)
 
     % Initialize with zeroed delta
     k0.P_dot = [0,0]; % Position Derivative
-    k0.V_dot = [0,0]; % Velocity Derivative thrust here 
+    k0.V_dot = [0,0]; % Velocity Derivative
     k0.O_dot = [0];   % Orientation Derivative
+    k0.B_dot = [0];   % Battery Drain
     k0.Time    = SimData.Time;
     %x = ThrustModel(SimData)
     % Calculate the k1 delta
@@ -24,11 +25,12 @@ function SimData = RK4_Integration(SimData)
     avg_delta.P_delta = dt*(k1.P_dot + 2*k2.P_dot + 2*k3.P_dot + k4.P_dot)/6;
     avg_delta.V_delta = dt*(k1.V_dot + 2*k2.V_dot + 2*k3.V_dot + k4.V_dot)/6;
     avg_delta.O_delta = dt*(k1.O_dot + 2*k2.O_dot + 2*k3.O_dot + k4.O_dot)/6;
+    avg_delta.B_delta = dt*(k1.B_dot + 2*k2.B_dot + 2*k3.B_dot + k4.B_dot)/6;
 
     % Update Simulation Time
     SimData.Time = SimData.Time + dt;
 
-    % Reset the altitude when we are on the ground to prevent the plane from 
+    % Reset the altitude when we are on the ground to prevent the plane from
     % going very slightly below the ground due to the discreteness of the simulation
     if SimData.Plane.FSM_state == 0
         SimData.StateVector.Position(2) = SimData.ground_height;
@@ -44,7 +46,7 @@ function SimData = RK4_Integration(SimData)
     SimData.Plane.FSM_state = Get_FSM_State(SimData);
     flight_path_angle = atan2d(SimData.StateVector.Velocity(2), SimData.StateVector.Velocity(1));
     SimData.Plane.AoA = SimData.StateVector.Orientation(1) - flight_path_angle;
-    
+
     SimData.battery_update = k4.battery_update;
 
 endfunction

efd97a43d90634c76f9b457d95edec900aaa49d5
Raul Maldonado <raul.maldonado83@gmail.com>
2021-01-23 13:26:13 -0600

    Made the Gravity Model into LF explicitly. Added parentheses to the getControlsVector call

--- Simulation/Models/GravityModel_Generator.m
+++ Simulation/Models/GravityModel_Generator.m
@@ -1,6 +1,6 @@
 function Gen_GravityModel = GravityModel_Generator(Magnitude,Direction)
 
-  Gen_GravityModel = @GravityModel;  
+  Gen_GravityModel = @GravityModel;
 
   function Dervs = GravityModel(stateVector,controlsVector)
     SV_Index = StateVector_Index;

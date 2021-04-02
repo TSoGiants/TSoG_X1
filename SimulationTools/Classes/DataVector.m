60833b1807dc44f62b1873efb30404686f03497f
Raul Maldonado <raul.maldonado83@gmail.com>
2021-04-02 13:36:21 -0500

    Initial commit of new DataVector class

---
+++ SimulationTools/Classes/DataVector.m
@@ -0,0 +1,14 @@
+classdef DataVector
+  properties
+    
+  endproperties
+  
+  methods
+    % Create Data Vector Object
+    function DV = DataVector(DataArray)
+      if ~iscell(DataArray)
+        
+      endif
+      
+    endfunction
+  endmethods
